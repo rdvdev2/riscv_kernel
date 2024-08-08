67eb1e6a17bf7f9291e4f94244ac42273acbce22
Nara Díaz Viñolas <rdvdev2@gmail.com>
2024-08-09 00:53:46 +0200

    Migrate to OpenSBI

--- build.rs
+++ build.rs
@@ -3,6 +3,8 @@ fn main() {
         .file("src/entry.S")
         .asm_flag("-march=rv64gc")
         .compile("entry");
+    println!("cargo::rerun-if-changed=src/entry.S");
 
     println!("cargo::rustc-link-arg=-Tsrc/linker.ld");
+    println!("cargo::rerun-if-changed=src/linker.ld");
 }
